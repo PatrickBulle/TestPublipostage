178c4425d347e03f752e55f48d94f004c92ab7a9
Patrick BULLE <patrick.bulle@synergie-est.fr>
2020-07-17 10:03:45 +0200

    Ajoutez des fichiers projet.

---
+++ TestPublipostage/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestPublipostage
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string modele = @"C:\Users\PBulle\Desktop\ModèleLettreGagnant.docx";
+            const string documentGenere = @"C:\Users\PBulle\Desktop\Resultat.pdf";
+            Dictionary<string, string> dictionnaire = new Dictionary<string, string>();
+            List<int> valeurs = new List<int>();
+
+            dictionnaire.Add("#VILLE_ENTETE#", "Besançon");
+            dictionnaire.Add("#DATE_ENTETE#", "Jeudi 16 Juillet 2020");
+            dictionnaire.Add("#CIVILITE#", "Monsieur");
+            dictionnaire.Add("#NOM#", "GARNIER");
+            dictionnaire.Add("#PRENOM#", "Yan");
+
+            for (int i = 0; i < 12; i++)
+            {
+                valeurs.Add(i);
+            }
+            
+            IPublipostage pub = new Publipostage();
+            pub.FaitLeTaf(modele, documentGenere, dictionnaire, valeurs);
+        }
+    }
+}
---
+++ TestPublipostage/Publipostage.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPublipostage
+{
+    class Publipostage : IPublipostage
+    {
+        public void FaitLeTaf(string cheminEtNomDuModele, string cheminEtNomDuDocumentGenere, Dictionary<string, string> dictionnaire, List<int> valeursDuGraphe)
+        {
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            Document document = app.Documents.Open(cheminEtNomDuModele);
+
+            foreach (KeyValuePair<string, string> entree in dictionnaire)
+            {
+                Remplace(document, entree);
+            }
+
+            GestionDuGraphe(document, valeursDuGraphe);
+
+            document.ExportAsFixedFormat(cheminEtNomDuDocumentGenere, WdExportFormat.wdExportFormatPDF);
+            // Fermer sans enregistrer
+            document.Close(false);
+            app.Quit();
+        }
+
+        private void GestionDuGraphe(Document document, List<int> valeursDuGraphe)
+        {
+            foreach (InlineShape inlineShape in document.InlineShapes)
+            {
+                if (inlineShape.HasChart == MsoTriState.msoTrue)
+                {
+                    Microsoft.Office.Interop.Word.Chart graphique = inlineShape.Chart;
+
+                    Workbook wb = graphique.ChartData.Workbook;
+                    Worksheet ws = wb.Worksheets["Feuil1"];
+
+                    ws.Range["B2"].Value = valeursDuGraphe[0];
+                    ws.Range["B2"].Value = valeursDuGraphe[1];
+                    ws.Range["C3"].Value = valeursDuGraphe[2];
+                    ws.Range["D4"].Value = valeursDuGraphe[3];
+                    graphique.Refresh();
+                    graphique = null;
+
+                    // Pour fermer proprement Excel
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(ws);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(wb);
+
+                    ws = null;
+                    wb = null;
+                }
+            }
+
+        }
+
+        private void Remplace(Document document, KeyValuePair<string, string> entree)
+        {
+            foreach (Microsoft.Office.Interop.Word.Range range in document.StoryRanges)
+            {
+                Find find = range.Find;
+                //options
+                object matchCase = false;
+                object matchWholeWord = true;
+                object matchWildCards = false;
+                object matchSoundsLike = false;
+                object matchAllWordForms = false;
+                object forward = true;
+                object format = false;
+                object matchKashida = false;
+                object matchDiacritics = false;
+                object matchAlefHamza = false;
+                object matchControl = false;
+                object read_only = false;
+                object visible = true;
+                object replace = WdReplace.wdReplaceAll;
+                object wrap = 1;
+                //execute find and replace
+                find.Execute(entree.Key, ref matchCase, ref matchWholeWord,
+                    ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, entree.Value, ref replace,
+                    ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+            }
+        }
+    }
+}
